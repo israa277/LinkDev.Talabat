24e691009a4d72917d313a93f2a42c1ff7f1fe11
IsRaa Ahmed <IsRaa Ahmed@DESKTOP-GPAPH8D>
2024-10-09 15:24:09 +0300

    Part 02 SaveChanges and LoggedUserService

--- LinkDev.Talabat.Infrastructure.Persistence/DependencyInjection.cs
+++ LinkDev.Talabat.Infrastructure.Persistence/DependencyInjection.cs
@@ -19,6 +19,7 @@ namespace LinkDev.Talabat.Infrastructure.Persistence
 			services.AddScoped<IStoreContextInitializer, StoreContextInitializer>();
 			services.AddScoped(typeof(IStoreContextInitializer), typeof(StoreContextInitializer));
 			services.AddScoped(typeof(ISaveChangesInterceptor), typeof(CustomSaveChangesInterceptor));
+
 			return services;
 
 
