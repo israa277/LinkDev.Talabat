02638efaa91aafd0bcafcfca52cf10b9fa1a5b42
IsRaa Ahmed <IsRaa Ahmed@DESKTOP-GPAPH8D>
2024-10-09 17:22:36 +0300

    Part 04 Service Manager

--- LinkDev.Talabat.Core.Applicarion/DependencyInjection.cs
+++ LinkDev.Talabat.Core.Applicarion/DependencyInjection.cs
@@ -1,16 +1,18 @@
 ﻿using LinkDev.Talabat.Core.Applicarion.Mapping;
 using LinkDev.Talabat.Core.Application.Abstraction.Services;
+using LinkDev.Talabat.Core.Application.Abstraction.Services.Products;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace LinkDev.Talabat.Core.Applicarion
 {
-	public static class DependencyInjection
+    public static class DependencyInjection
 	{
 		public static IServiceCollection AddApplicationServices(this IServiceCollection services)
 
 		{
 			services.AddAutoMapper(typeof(MappingProfile));
-			services.AddScoped(typeof(IProductService), typeof(IProductService));
+			//services.AddScoped(typeof(IProductService), typeof(IProductService));
+			services.AddScoped(typeof(IServiceManager), typeof(IServiceManager));
 			return services;
 
 		}
