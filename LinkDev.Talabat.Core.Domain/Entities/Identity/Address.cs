c96519396e322d1ceeee15163a99b826f3f2dac8
IsRaa Ahmed <IsRaa Ahmed@DESKTOP-GPAPH8D>
2024-10-17 19:17:27 +0300

    Part 01 Identity Module - Entities

---
+++ LinkDev.Talabat.Core.Domain/Entities/Identity/Address.cs
@@ -0,0 +1,14 @@
+﻿namespace LinkDev.Talabat.Core.Domain.Entities.Identity
+{
+	public class Address 
+	{
+		public int Id { get; set; }
+		public required string FirstName { get; set; }
+		public required string LastName { get; set; }
+		public required string Street { get; set; }
+		public required string City { get; set; }
+		public required string Country { get; set; }
+		public int UserId { get; set; }
+		public required ApplicationUser AppUser { get; set; }
+	}
+}
