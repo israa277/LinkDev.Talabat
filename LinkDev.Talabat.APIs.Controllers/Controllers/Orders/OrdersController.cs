f8b26a85f13d6be15ee72c3890d5bf8b6801002c
IsraaAhmed <IsraaAhmed@ISRAAAHMED>
2024-11-23 23:13:41 +0200

    Part 08 GetOrdersForUser, GetOrders, GetDeliveryMethods endpoints

--- LinkDev.Talabat.APIs.Controllers/Controllers/Orders/OrdersController.cs
+++ LinkDev.Talabat.APIs.Controllers/Controllers/Orders/OrdersController.cs
@@ -17,5 +17,29 @@ namespace LinkDev.Talabat.APIs.Controllers.Controllers.Orders
             var result = await serviceManager.OrderService.CreateOrderAsync(buyerEmail!,orderDto);
             return Ok(result);
         }
+
+        [HttpGet] // GET: /api/Orders
+        public async Task<ActionResult<IEnumerable<OrderToReturnDto>>> GetOrdersForUser()
+        {
+            var buyerEmail = User.FindFirstValue(ClaimTypes.Email);
+            var result = await serviceManager.OrderService.GetOrderForUserAsync(buyerEmail!);
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")] // GET: /api/Orders/{id}
+        public async Task<ActionResult<OrderToReturnDto>> GetOrders(int id)
+        {
+            var buyerEmail = User.FindFirstValue(ClaimTypes.Email);
+            var result = await serviceManager.OrderService.GetOrderByIdAsync(buyerEmail!,id);
+            return Ok(result);
+        }
+
+
+        [HttpGet("deliveryMethods")] // GET: /api/Orders/deliveryMethods
+        public async Task<ActionResult<IEnumerable<DeliveryMethodDto>>> GetDeliveryMethods()
+        {
+            var result = await serviceManager.OrderService.GetDeliveryMethodAsync();
+            return Ok(result);
+        }
     }
 }
